778bdb2e45fa828bf6ffd7ee958333a8045674e9
PedroHenrique-git <pedromf34@outlook.com>
2022-01-09 17:19:58 -0300

    'createReducer'

---
+++ utils/myCreateReducer.ts
@@ -0,0 +1,5 @@
+export const createReducer = (initialState, handleActions) =>
+    (state = initialState, action) => 
+        handleActions.hasOwnProperty(action.type) ?
+          handleActions[action.type](state, action) :
+            state;
